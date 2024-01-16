2d4e6e053687eedb7f1cb1abf70a815d7e505e11
D3nd1s <d3nd1smilupa@gmail.com>
2024-01-16 20:57:55 +0200

    test commit #1

---
+++ Snusick.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	int a = 0;
+	for (int i = 2; i < 10; i++)
+	{
+		std::cout << "i = " << i << std::endl;
+		a++; 
+	}
+	std::cout << a;
+
+}
+
+// ctr + K + ctr + D - форматування тексту, вирівнювання операторів наприклад : (a+b) -> (a + b)
